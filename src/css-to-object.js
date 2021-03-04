99902bcf9921f2b32c0380fbf42fcbff074dac97
UpperCod <mtrujillo@outlook.cl>
2021-03-04 11:43:05 -0300

    save branch

--- src/css-to-object.js
+++ src/css-to-object.js
@@ -1,72 +1,49 @@
 /**
  *
- * @param {string} css
+ * @param {string} content
  */
-export function cssToObject(css) {
-    css = replace(
-        `:host{${css}}`,
-        /**
-         * Clean line breaks and normalize spaces
-         */
-        [/\s+/g, " "],
-        /**
-         * delete comments
-         */
-        [/\/\*.+?\*\//g, ""],
-        /**
-         * Replace double characters with single characters
-         */
-        [/\"/g, `'`],
-        /**
-         * Add ";" in case of css compression
-         * @example
-         * color: black} >> color: black;}
-         */
-        [/:([^;}]+)}/g, `:$1;}`],
-        /**
-         * uncomment to add import support
-         * [/(@import +[^;]+);/g,`$1{}`],
-         */
-        /**
-         * Capture the selectors and generate an index block for the JSON
-         * @example
-         * .selector{} >> ".selector":{}
-         */
-        [/ *([^;{}]+) *{/g, `"$1":{`],
-        /**
-         * Capture the css blocks "{}", to transform the css props into json props
-         * @example
-         * {width:200px;} >> {"width":"200px"}
-         */
-        [/{[^{}]+}/g, props],
-        /**
-         * Capture the first excludi block when using nested, to transform css props into json props
-         * @example
-         * {width:200px;&:hover{color:red;}} >> {"width":"200px"}
-         */
-        [/{([^"]+)/g, props],
-        /**
-         * Fix the union of props
-         */
-        [/}"/g, `},"`],
-        /**
-         * Fix props separation
-         */
-        [/, *}/g, `}`]
+const props = (content) =>
+    content.replace(
+        /\s*([^:]+)\s*:\s*([^;]+)(;){0,1}/g,
+        (all, prop, value) => `"${prop}":"${encode(value)}",`
     );
-    return JSON.parse(`{${css}}`);
+/**
+ *
+ * @param {string} content
+ */
+const encode = (content) => content.replace(/"/g, '\\"');
+/**
+ *
+ * @param {string} selector
+ * @param {string[]} content
+ */
+const rule = (selector, content) => `${selector}{${content.join(";")}}`;
+
+/**
+ *
+ * @param {string} style
+ */
+export function cssToObject(style) {
+    const items = [];
+    let test;
+    while ((test = style.match(/(\<\d+){0,1}\s*([^};]+)\{([^{}]+)\}/))) {
+        const [all, mark, selector, content] = test;
+        style = style.replace(
+            all,
+            `<${items.push([encode(selector), props(content), mark]) - 1}`
+        );
+    }
+    style = props(style);
+    while ((test = style.match(/<(\d+)/))) {
+        const [mark, id] = test;
+        const [selector, content, beforeMark = ""] = items[id];
+        style = style.replace(mark, `${beforeMark}"${selector}":{${content}},`);
+    }
+
+    style = `{${style}}`
+        .replace(/\n+/g, "")
+        .replace(/,\s*}/g, "}")
+        .replace(/,$/, "");
+
+    return JSON.parse(style);
 }
-
-/**
- * formats properties as JSON
- * @param {string} block
- */
-const props = (block) => block.replace(/([\w-]+) *: *([^;]+);/g, `"$1":"$2",`);
-
-/**
- * execute multiple replace on a string based on the given arguments
- * @param {string} str
- * @param {any[]} args
- */
-const replace = (str, ...args) =>
-    args.reduce((str, args) => str.replace(...args), str);
