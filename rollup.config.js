8527a53cf38ad1c736cfbbd683640e79a50db357
UpperCod <mtrujillo@outlook.cl>
2021-03-04 11:38:18 -0300

    fix .d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -9,5 +9,4 @@ export default {
             sourcemap: true,
         },
     ],
-    plugins: [resolve()],
 };
--- types.d.ts
+++ types.d.ts
@@ -6,5 +6,5 @@ declare module "@uppercod/css-to-object" {
     export interface Host extends CSSObjectFill {
         ":host": CSSObjectFill;
     }
-    export function cssToJson(css: string): Host;
+    export function cssToObject(css: string): Host;
 }
