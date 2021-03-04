05c0a6d83c99147f247984a467d4d6a155d5b403
UpperCod <mtrujillo@outlook.cl>
2021-03-04 10:59:45 -0300

    Improve package export

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,13 @@
-import resolve from "@rollup/plugin-node-resolve";
-import builtins from "builtin-modules";
 import pkg from "./package.json";
 
 export default {
     input: ["./css-to-object.js"],
     output: [
         {
-            dir: "cjs",
+            file: "css-to-object.cjs",
             format: "cjs",
             sourcemap: true,
         },
     ],
-    external: [...builtins, ...Object.keys(pkg.dependencies)],
     plugins: [resolve()],
 };
